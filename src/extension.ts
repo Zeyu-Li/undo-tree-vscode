e95911e8cf262f82faebb3ee1eccf486a6b22927
Kaaden RumanCam <kaadenruman@hotmail.com>
2024-02-29 22:14:45 -0500

    detect event

--- src/extension.ts
+++ src/extension.ts
@@ -58,8 +58,6 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		
-		// vscode.window.showInformationMessage('Hello World from undo-tree!');
-
 		// TODO: read undoTree from disk
 
 		vscode.workspace.onDidChangeTextDocument((event) => {
@@ -87,7 +85,8 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
